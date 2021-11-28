26a1a35a8563f9b295a96dbebfe12ae0dbfaebeb
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-11-28 12:37:02 -0800

    added the label fonts and imported the images

--- diceRoller.py
+++ diceRoller.py
@@ -3,10 +3,31 @@
 # Dice Roller using python, includes a GUI
 
 import tkinter
-import PIL import Image, ImageTk
+from PIL import Image
+from PIL import ImageTk
 import random
 
+
+
 # main window of the app, top level widget which shows the window
-root = tkinter
+root = tkinter.Tk()
 root.geometry('400x400')
 root.title('Dice Roller in Python')
+
+# keep the window open and call the mainloop of Tk
+root.mainloop()
+
+# Adds the label into the frame
+blankLine = tkinter.Label(root, text="")
+blankLine.pack()
+
+# adds a label with different font and formating
+labelHeading  = tkinter.Label(root, text=
+"Start the Symulation",
+    fg = "light green",
+        bg = "dark green",
+            font = "Helvetica 16 bold italic")
+labelHeading.pack()
+
+# add the images from the main dirrectory
+dice = ['die1.png', 'die2.png', 'die3.png', 'die4.png', 'die5.png', 'die6.png']
