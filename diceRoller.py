546e73d3d772e52b35e1c24399fabbacd51bf1aa
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-11-28 13:14:34 -0800

    draft

--- diceRoller.py
+++ diceRoller.py
@@ -1,35 +1,29 @@
 # Ruben Sanduleac
 # November 28th, 2020
-# Dice Roller using python, includes a GUI
+# Dice Roller using python, uses the tkinter and PIL library for the GUI
+# the programcreates the window where labels are create and external dice images are imported. 
+# the random library is used to cycle through the images to randomly stop at image. The user inities the program 
+# by pressing the 'roll' button.
 
 import tkinter
 from PIL import Image
 from PIL import ImageTk
 import random
 
-
-
 # main window of the app, top level widget which shows the window
 root = tkinter.Tk()
-root.geometry('400x400')
-root.title('Dice Roller in Python')
-
-# keep the window open and call the mainloop of Tk
-root.mainloop()
+root.geometry('500x500')
+root.title('Ruben Sanduleac: Dice Roller')
 
 # Adds the label into the frame
 blankLine = tkinter.Label(root, text="")
 blankLine.pack()
 
 # adds a label with different font and formating
-labelHeading  = tkinter.Label(root, text=
-"Start the Symulation",
-    fg = "light green",
-        bg = "dark green",
-            font = "Helvetica 16 bold italic")
+labelHeading  = tkinter.Label(root, text="Start the Symulation", fg = "Black", font = "Times 24 bold italic")
 labelHeading.pack()
 
-# add the images from the main dirrectory
+# add the images from the main directory
 dice = ['die1.png', 'die2.png', 'die3.png', 'die4.png', 'die5.png', 'die6.png']
 
 # simulate the dice with random integers between 0 and 6 and generate an image for the user
@@ -39,5 +33,22 @@ dicePicture = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 labelImage = tkinter.Label(root, image=dicePicture)
 labelImage.image = dicePicture
 
-# pack the widnget in the parent widget
+# packing a widget in the parent widget 
 labelImage.pack(expand=True)
+
+# function updates the image while keeping the reference during the roll
+def rollDice():
+    dicePicture = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    # update image
+    labelImage.configure(image=dicePicture)
+    # keep a reference
+    labelImage.image = dicePicture
+
+# adds a button upon the users press to roll the fice
+button = tkinter.Button(root, text='Roll', fg='black', command=rollDice)
+
+# pack a widget in the parent widget
+button.pack( expand=True)
+
+# keep the window open and call the mainloop of Tk
+root.mainloop()
