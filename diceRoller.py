18f9005dfc3b7b7cf4e7aabfbe2da8471e651307
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-11-28 12:14:46 -0800

    first commit

---
+++ diceRoller.py
@@ -0,0 +1,12 @@
+# Ruben Sanduleac
+# November 28th, 2020
+# Dice Roller using python, includes a GUI
+
+import tkinter
+import PIL import Image, ImageTk
+import random
+
+# main window of the app, top level widget which shows the window
+root = tkinter
+root.geometry('400x400')
+root.title('Dice Roller in Python')
